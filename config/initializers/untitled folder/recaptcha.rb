2114486a0672f483bc21403229763fedc1acdc21
Steve Downey <steve.downtown@gmail.com>
2012-08-11 13:28:07 -0700

    minor ui, spec tweaks

--- config/initializers/untitled folder/recaptcha.rb
+++ config/initializers/untitled folder/recaptcha.rb
@@ -1,5 +1,4 @@
 Recaptcha.configure do |config|
   config.public_key  = '6LesItUSAAAAADxBHQrFwVFUbN57L0VTQGvdEV6-'
   config.private_key = '6LesItUSAAAAAMXtaqmkcYDN2VHEqVaqjX7vz7rs'
-  # config.proxy = 'http://myproxy.com.au:8080'
 end
--- spec/integration/feedbacks_controller_spec.rb
+++ spec/integration/feedbacks_controller_spec.rb
@@ -14,9 +14,17 @@ describe FeedbacksController do
       Feedback.first.body.should == 'my feedback'
     end
     
-    it "failure" do
+    it "failure -validation" do
       click_on 'submit'
       page.should have_content("Enter something in Subject or Body")
+      Feedback.count.should == 0
+    end
+    
+    it "failure - recaptcha" do
+      FeedbacksController.any_instance.stub(:verify_recaptcha).and_return(false)
+      fill_in 'feedback_body', :with => 'my feedback'
+      click_on 'submit'
+      page.should_not have_content("Thank you for your feedback")
       Feedback.count.should == 0
     end
   end
