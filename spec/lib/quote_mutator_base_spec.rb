fc46d75897d959fbfe6b524e34501e2fc5449875
Steve Downey <steve.downtown@gmail.com>
2012-08-16 09:05:57 -0700

    refactor quote_mutator_base

--- spec/lib/quote_mutator_base_spec.rb
+++ spec/lib/quote_mutator_base_spec.rb
@@ -7,17 +7,6 @@ describe QuoteMutatorBase do
   let(:admin) { FactoryGirl.create(:admin_user)}
   let(:qm_user) { QuoteMutatorBase.new(user)}
   let(:qm_admin) { QuoteMutatorBase.new(admin) }
-  let(:quote_attributes) { {:who => 'who', :text => 'what'} }
-  
-  describe '.initialize' do
-    it "sets user" do
-      qm_user.user.should == user
-    end
-    
-    it "raise error if no user passed" do
-      expect { QuoteMutatorBase.new('') }.to raise_error(QuoteMutatorBase::NoUserError)
-    end
-  end
   
   describe '#find_quote' do
     it "admin" do
@@ -31,38 +20,10 @@ describe QuoteMutatorBase do
     end
   end
   
-  describe '#success?' do
-    it "true" do
-      qm_user.success = true
-      qm_user.should be_success
-    end
-    
-    it "false" do
-      qm_user.success = false
-      qm_user.should_not be_success
-    end
-    
-    it "error" do
-      expect { qm_user.success? }.to raise_error(QuoteMutatorBase::SuccessCalledBeforeMutatorCalledError)
-    end
-  end
-  
   describe '#qwa' do
     it "has quote" do
       qm_user.quote = quote
       qm_user.qwa.quote = quote
-    end
-  end
-  
-  describe '#admin?' do
-    it 'admin' do
-      qm_admin.admin?.should be_true
-      qm_admin.not_admin?.should be_false
-    end
-    
-    it "non-admin" do
-      qm_user.admin?.should be_false
-      qm_user.not_admin?.should be_true
     end
   end
   
